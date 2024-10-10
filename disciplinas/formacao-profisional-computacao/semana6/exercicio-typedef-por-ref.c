ce21afb9807d08eeb9cce3bcf402a7ee34839874
Sarah <sdmguerre@gmail.com>
2024-10-10 19:36:58 -0300

    revisitando conceitos em c

---
+++ disciplinas/formacao-profisional-computacao/semana6/exercicio-typedef-por-ref.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+typedef struct data{int Dia, Mes, Ano;}
+    DATA;
+
+typedef struct pessoa{
+    char Nome[100];
+    int Idade;
+    float Salario;
+    DATA Nasc;
+}   PESSOA;
+
+void Mostrar(struct pessoa *x) {
+
+    printf("Nome: %s\n", x->Nome);
+    printf("Idade: %d\n", x->Idade);
+    printf("Salario: %.2f\n", x->Salario);
+    printf("Data de Nascimento: %d/%d/%d\n", x->Nasc.Dia, x->Nasc.Mes, x->Nasc.Ano);
+}
+
+main(){
+
+    struct pessoa *p = malloc(sizeof(struct pessoa));
+    *p = (struct pessoa) {"Sarah", 28, 500.00,{28,03,1996}};
+    
+    Mostrar(p);
+    free(p);
+}
+
---
+++ disciplinas/formacao-profisional-computacao/semana6/exercicio-typedef.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct {int Dia, Mes, Ano;}
+    DATA;
+
+typedef struct pessoa{
+    char Nome[100];
+    int Idade;
+    float Salario;
+    DATA Nasc;
+}   PESSOA;
+
+void Mostrar(struct pessoa x) {
+
+    printf("Nome: %s\n", x.Nome);
+    printf("Idade: %d\n", x.Idade);
+    printf("Salario: %.2f\n", x.Salario);
+    printf("Data de Nascimento: %d/%d/%d\n", x.Nasc.Dia, x.Nasc.Mes, x.Nasc.Ano);
+}
+
+main(){
+
+    struct pessoa p = {"Sarah", 28, 500.00,{28,03,1996}};
+
+    Mostrar(p);
+}
+
